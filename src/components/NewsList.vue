7815a3d9fa39f451ff3dcf143d9116023ceee424
Ezeigbo Chimaobi Emmanuel <echimaobi5@gmail.com>
2018-02-14 11:19:55 +0100

    Implemented template section of NewsList.vue component

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div class="newslist">
+    <div class="container">
+      <ul class="media-list">
+        <li class="media" v-for="article in articles">
+          <div class="media-left">
+            <a v-bind:href="article.url" target="_blank">
+              <img class="media-object" v-bind:src="article.urlToImage">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading"><a v-bind:href="article.url" target="_blank">{{article.title}}</a></h4>
+            <h5><i>by {{article.author}}</i></h5>
+            <p>{{article.description}}</p>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
