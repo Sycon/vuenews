051e7176fa647233185c057de632a2a4e47b22f6
Ezeigbo Chimaobi Emmanuel <echimaobi5@gmail.com>
2018-02-14 10:51:16 +0100

    Implemented the template of SourceSelection.vue component and renamed its "method" to "methods"

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="sourceselection">
+    <div class="container">
+      <div class="jumbotron">
+        <h3><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Sycon News List</h3>
+        <h4>Select News Source</h4>
+        <select class="form-control" v-on:change="sourceChanged">
+          <option value="">Please Select News Source</option>
+          <option v-for="source in sources" v-bind:value="source.id">{{source.name}}</option>
+        </select>
+        <div v-if="source">
+          <h6>{{source.description}}</h6>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +25,7 @@
         source: ''
       }
     },
-    method: {
+    methods: {
       sourceChanged: function (e) {
         for (let i = 0; i < this.sources.length; i++) {
           if (this.sources[i].id === e.target.value) {
@@ -25,7 +38,7 @@
     created: function () {
       this.$http.get('https://newsapi.org/v1/sources?language=en')
         .then(response => {
-          this.source = response.data.sources
+          this.sources = response.data.sources
         })
     }
   }
