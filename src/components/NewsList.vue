74abd6e8c045c557126e03b408ab185407842e82
Ezeigbo Chimaobi Emmanuel <echimaobi5@gmail.com>
2018-02-14 11:06:00 +0100

    Implemented script section of NewsList.vue component

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -7,9 +7,26 @@
 <script>
   export default {
     name: 'news_list',
+    props: ['source'],
     data () {
       return {
-        msg: 'NewsList'
+        articles: []
+      }
+    },
+    methods: {
+      updateSource: function (source) {
+        this.$http.get('https://newsapi.org/v1/articles?source=' + source + '&apiKey=d6e8ee2cb7ee4f4c81da7e9c9aeb89be')
+          .then(response => {
+            this.articles = response.data.articles
+          })
+      }
+    },
+    created: function () {
+      this.updateSource(this.source)
+    },
+    watch: {
+      source: function (val) {
+        this.updateSource(val)
       }
     }
   }
